00470cfe62ea9ff74405138ae68a9e499392e13f
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-07-02 01:34:28 +0430

    [utils][os_utils] Add os&pass utils

---
+++ deep_utils/utils/os_utils/__init__.py
@@ -0,0 +1,1 @@
+from .os_path import is_image_file, split_all
--- deep_utils/utils/os_utils/os_path.py
+++ deep_utils/utils/os_utils/os_path.py
@@ -1,3 +1,5 @@
+import os
+
 IMG_EXTENSIONS = [
     '.jpg', '.JPG', '.jpeg', '.JPEG',
     '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP', '.tiff'
@@ -6,3 +8,19 @@ IMG_EXTENSIONS = [
 
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+def split_all(path):
+    all_parts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            all_parts.insert(0, parts[0])
+            break
+        elif parts[1] == path:  # sentinel for relative paths
+            all_parts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            all_parts.insert(0, parts[1])
+    return all_parts
