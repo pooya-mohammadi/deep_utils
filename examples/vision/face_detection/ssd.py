fe1bb2512fb954ebef23f4c95824df3c657f71a8
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-07-06 17:12:18 +0430

    Add face detection examples

---
+++ examples/vision/face_detection/ssd.py
@@ -0,0 +1,11 @@
+import cv2
+from deep_utils import Box, show_destroy_cv2
+from deep_utils import face_detector_loader
+
+if __name__ == '__main__':
+    face_detector = face_detector_loader('SSDCV2CaffeFaceDetector')
+    img = cv2.imread('../data/pooya.jpg')
+    boxes, confidences, elapsed_time = face_detector.detect_faces(img, get_time=True)
+    img = Box.put_box(img, boxes[0], )
+    print("boxes:", boxes, "confidences: ", confidences, "elapsed_time: ", elapsed_time)
+    show_destroy_cv2(img)
