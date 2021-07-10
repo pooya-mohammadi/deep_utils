95f1b3b4249a3b89879178b7a6e7ad753a7f4874
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-07-11 00:36:31 +0430

    Change data pic

--- examples/vision/face_detection/ssd.py
+++ examples/vision/face_detection/ssd.py
@@ -4,7 +4,7 @@ from deep_utils import face_detector_loader
 
 if __name__ == '__main__':
     face_detector = face_detector_loader('SSDCV2CaffeFaceDetector')
-    img = cv2.imread('../data/pooya.jpg')
+    img = cv2.imread('../data/movie-stars.jpg')
     boxes, confidences, elapsed_time = face_detector.detect_faces(img, get_time=True)
     img = Box.put_boxes(img, boxes)
     print("boxes:", boxes, "confidences: ", confidences, "elapsed_time: ", elapsed_time)
