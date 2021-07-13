6f36807388e0dc12ba1d6bc9ef518ce4f56a2575
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-07-13 15:45:05 +0430

    [examples][face_detection] Provide is_rgb argument

--- examples/vision/face_detection/ssd.py
+++ examples/vision/face_detection/ssd.py
@@ -5,7 +5,7 @@ from deep_utils import face_detector_loader
 if __name__ == '__main__':
     face_detector = face_detector_loader('SSDCV2CaffeFaceDetector')
     img = cv2.imread('../data/movie-stars.jpg')
-    boxes, confidences, elapsed_time = face_detector.detect_faces(img, get_time=True)
-    img = Box.put_boxes(img, boxes)
-    print("boxes:", boxes, "confidences: ", confidences, "elapsed_time: ", elapsed_time)
+    res = face_detector.detect_faces(img, is_rgb=False, get_time=True)
+    img = Box.put_boxes(img, res['boxes'])
+    print("boxes:", res['boxes'], "confidences: ", res['confidences'], "elapsed_time: ", res['elapsed_time'])
     show_destroy_cv2(img)
