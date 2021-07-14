3b6214f7d15d02c22540fbdcc974ab39a0c6cd0a
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-07-14 13:13:25 +0430

    Rename main_abs Module

--- deep_utils/utils/box_utils/__init__.py
+++ deep_utils/utils/box_utils/__init__.py
@@ -1,3 +1,3 @@
-from deep_utils.utils.lib_utils.import_utils import import_module
+from deep_utils.utils.lib_utils.main_utils import import_module
 
 Box = import_module("deep_utils.utils.box_utils.boxes", "Box")
