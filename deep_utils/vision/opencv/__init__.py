cfc317279ecb6edc3c75c82fbb6da657eb730f9d
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-07-14 13:21:26 +0430

    [utils] Rename Module

--- deep_utils/vision/opencv/__init__.py
+++ deep_utils/vision/opencv/__init__.py
@@ -1,3 +1,3 @@
-from deep_utils.utils.lib_utils.import_utils import import_module
+from deep_utils.utils.lib_utils.main_utils import import_module
 
 show_destroy_cv2 = import_module("deep_utils.vision.opencv.main", "show_destroy_cv2")
