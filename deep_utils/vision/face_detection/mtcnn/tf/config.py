4d6cc2bcde32bb3cad354486cc21b341c2e4ed09
Vargha <vkhalokhi@gmail.com>
2021-08-15 16:44:07 +0430

    [vision][face_detection][mtcnn][tf] Fixing keras models directory in config file

--- deep_utils/vision/face_detection/mtcnn/tf/config.py
+++ deep_utils/vision/face_detection/mtcnn/tf/config.py
@@ -6,11 +6,11 @@ class Config:
     min_detection_size = 12
     factor = 0.707
     pnet_url = "https://github.com/Practical-AI/deep_utils/releases/download/0.1.0/pnet.h5"
-    pnet_cache = 'weights/vision/face_detection/mtcnn/tf/pnet.npy'
+    pnet_cache = 'weights/vision/face_detection/mtcnn/tf/pnet.h5'
     pnet = None
     rnet_url = "https://github.com/Practical-AI/deep_utils/releases/download/0.1.0/rnet.h5"
-    rnet_cache = 'weights/vision/face_detection/mtcnn/tf/rnet.npy'
+    rnet_cache = 'weights/vision/face_detection/mtcnn/tf/rnet.h5'
     rnet = None
     onet_url = "https://github.com/Practical-AI/deep_utils/releases/download/0.1.0/onet.h5"
-    onet_cache = 'weights/vision/face_detection/mtcnn/tf/onet.npy'
+    onet_cache = 'weights/vision/face_detection/mtcnn/tf/onet.h5'
     onet = None
