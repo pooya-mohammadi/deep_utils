09688d4ec325052459d78a2593bfa23ea69ebeea
Vargha <vkhalokhi@gmail.com>
2021-08-16 21:48:46 +0430

    [vision][face_detection][ultralight] Appending Ultralight Torch Model in face detection

---
+++ deep_utils/vision/face_detection/ultralight/__init__.py
@@ -0,0 +1,4 @@
+from deep_utils.utils.lib_utils import import_module
+
+UltralightTorchFaceDetector = import_module('deep_utils.vision.face_detection.ultralight.torch.ultralight_torch_face_detection',
+                                        'UltralightTorchFaceDetector')
---
+++ deep_utils/vision/face_detection/ultralight/torch/config.py
@@ -0,0 +1,8 @@
+class Config:
+    device = 'cpu'
+    RBF_url = ""
+    RBF_cache = 'weights/vision/face_detection/ultra-light/torch/RBF/version-RFB-320.pth'
+    RBF = None
+    slim_url = ""
+    slim_cache = 'weights/vision/face_detection/ultra-light/torch/slim/version-slim-320.pth'
+    slim = None
