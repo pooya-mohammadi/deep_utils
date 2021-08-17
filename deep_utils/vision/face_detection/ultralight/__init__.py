0658a33a0ad6124a600666ac8a266c3f6d5be13a
Vargha <vkhalokhi@gmail.com>
2021-08-17 15:10:36 +0430

    [vision][face_detection][ultralight] Appending ultralight tensorflow in the models in __init__ file

--- deep_utils/vision/face_detection/ultralight/__init__.py
+++ deep_utils/vision/face_detection/ultralight/__init__.py
@@ -4,6 +4,6 @@ UltralightTorchFaceDetector = import_module(
     'deep_utils.vision.face_detection.ultralight.torch.ultralight_torch_face_detection',
     'UltralightTorchFaceDetector')
 UltralightTFFaceDetector = import_module(
-    'deep_utils.vision.face_detection.ultralight.tf.ultralight_torch_face_detection',
+    'deep_utils.vision.face_detection.ultralight.tf.ultralight_tf_face_detection',
     'UltralightTFFaceDetector')
 
