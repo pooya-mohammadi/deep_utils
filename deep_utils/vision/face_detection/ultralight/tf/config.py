0d39fa60657747a3e81b79e8b69c375ecfa222ca
Vargha <vkhalokhi@gmail.com>
2021-08-17 15:32:09 +0430

    [vision][face_detection][ultralight] Appending Ultralight Tensorflow Model

---
+++ deep_utils/vision/face_detection/ultralight/tf/config.py
@@ -0,0 +1,7 @@
+class Config:
+    RBF_url = "https://github.com/Practical-AI/deep_utils/releases/download/0.2.0/RBF.zip"
+    RBF_cache = 'weights/vision/face_detection/ultra-light/tf/RBF/RBF.zip'
+    RBF = None
+    slim_url = "https://github.com/Practical-AI/deep_utils/releases/download/0.2.0/slim.zip"
+    slim_cache = 'weights/vision/face_detection/ultra-light/tf/slim/slim.zip'
+    slim = None
