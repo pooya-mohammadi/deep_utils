886b20bdc23c64ca03a8f75cdc761a8eff42dc49
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-08-17 11:09:21 +0430

    [utils][matplotlib][show-plt] Add fig-size

--- deep_utils/utils/matplotlib_utils/main.py
+++ deep_utils/utils/matplotlib_utils/main.py
@@ -1,6 +1,9 @@
-def show_plt(img):
+def show_plt(img, is_rgb, figsize=None):
     import matplotlib
     matplotlib.use('TkAgg')
     import matplotlib.pyplot as plt
+    if not is_rgb:
+        img = img[..., ::-1]
+    plt.figure(figsize=figsize)
     plt.imshow(img)
     plt.show()
