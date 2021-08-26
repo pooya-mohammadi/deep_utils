65da972cf0bf3484e7417c9ee1fda780aeaf1d3f
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-08-26 22:35:53 +0430

    [utils][resize-utils] Add an extra line to beautify

--- deep_utils/utils/resize_utils/__init__.py
+++ deep_utils/utils/resize_utils/__init__.py
@@ -1,5 +1,1 @@
-from deep_utils.utils.lib_utils.main_utils import import_module
-from .main_resize import get_img_shape
-
-resize = import_module("deep_utils.utils.resize_utils.main_resize", "resize")
-cv2_resize = import_module("deep_utils.utils.resize_utils.main_resize", "cv2_resize")
+from .main_resize import get_img_shape, resize, cv2_resize
