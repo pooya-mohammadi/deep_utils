0689c8c393485f7ea68bd568763ba6a229711c57
Vargha Khalokhi <vkhalokhi@gmail.com>
2021-09-08 18:41:11 +0430

    Fixing GPU config in TF MTCNN init file

--- deep_utils/vision/face_detection/mtcnn/tf/__init__.py
+++ deep_utils/vision/face_detection/mtcnn/tf/__init__.py
@@ -1,1 +1,6 @@
 from .mtcnn_tf_face_detection import MTCNNTFFaceDetector
+import tensorflow as tf
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) < 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
