25ceb068abea4882dc4952b71ebab136d9d3adf5
Vargha Khalokhi <vkhalokhi@gmail.com>
2021-09-08 18:39:18 +0430

    Fixing GPU config in TF ultralight init file

---
+++ deep_utils/vision/face_detection/ultralight/tf/__init__.py
@@ -0,0 +1,6 @@
+from .ultralight_tf_face_detection import UltralightTFFaceDetector
+import tensorflow as tf
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) < 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
