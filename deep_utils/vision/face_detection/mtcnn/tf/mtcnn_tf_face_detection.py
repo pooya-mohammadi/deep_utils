2574dddbc1869999ce265b9d4a08b850ddab6770
Vargha Khalokhi <vkhalokhi@gmail.com>
2021-09-10 15:09:22 +0430

    [vision][face_detection][mtcnn][tf] Fixing TF GPU config in MTCNN face detection

--- deep_utils/vision/face_detection/mtcnn/tf/mtcnn_tf_face_detection.py
+++ deep_utils/vision/face_detection/mtcnn/tf/mtcnn_tf_face_detection.py
@@ -9,10 +9,6 @@ from .src.box_utils import nms, calibrate_box, get_image_boxes, convert_to_squar
 from .src.first_stage import run_first_stage
 import tensorflow as tf
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
 
 class MTCNNTFFaceDetector(FaceDetector):
     def __init__(self, **kwargs):
@@ -21,6 +17,9 @@ class MTCNNTFFaceDetector(FaceDetector):
                          download_variables=("pnet", "onet", 'rnet'),
                          **kwargs)
         self.config: Config
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        if len(physical_devices) < 0:
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     @download_decorator
     def load_model(self):
