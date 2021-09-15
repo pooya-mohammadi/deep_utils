b3ed9b499ed85abe06be2d96f341c93ad44a0408
Vargha Khalokhi <vkhalokhi@gmail.com>
2021-09-15 17:09:00 +0430

    [vision][face_detection][retinaface] create retinaface torch model init file

---
+++ deep_utils/vision/face_detection/retinaface/__init__.py
@@ -0,0 +1,5 @@
+from deep_utils.utils.lib_utils import import_module
+
+RetinaFaceTorchFaceDetector = import_module(
+    'deep_utils.vision.face_detection.retinaface.torch.retinaface_torch_face_detection', 'RetinaFaceTorchFaceDetector'
+)
