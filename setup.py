f55c35078f1e07c2163ccc66e35abee8b52a3e70
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-11-01 16:10:35 +0330

    [setup] set version=0.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.install import install
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.3.0"
+VERSION = "0.4.0"
 
 
 class VerifyVersionCommand(install):
