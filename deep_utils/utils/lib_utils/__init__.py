e6ef4fb201a1b737f48c838c9fa36e2049a8c146
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-11-01 14:15:19 +0330

    [utils][lib_utils][download_utils] Add a new download module

--- deep_utils/utils/lib_utils/__init__.py
+++ deep_utils/utils/lib_utils/__init__.py
@@ -1,3 +1,3 @@
-from .download_utils import download_decorator
+from .download_utils import download_decorator, download_file
 from .main_utils import import_module
 from .lib_decorators import get_from_config, expand_input, get_elapsed_time, rgb2bgr, cast_kwargs_dict
--- deep_utils/utils/lib_utils/download_utils.py
+++ deep_utils/utils/lib_utils/download_utils.py
@@ -1,7 +1,7 @@
-import requests
 import os
 import sys
 from functools import wraps
+import requests
 
 
 def get_file(fname,
@@ -66,6 +66,49 @@ def get_file(fname,
     return fpath
 
 
+def download_file(url, file_path, unzip=False):
+    datadir, fname = os.path.split(file_path)
+    os.makedirs(datadir, exist_ok=True)
+    print('Downloading data from', url)
+    error_msg = 'URL fetch failure on {}'
+    try:
+        with open(file_path, 'wb') as f:
+            response = requests.get(url, stream=True)
+            total = response.headers.get('content-length')
+
+            if total is None:
+                f.write(response.content)
+            else:
+                downloaded = 0
+                total = int(total)
+                for data in response.iter_content(chunk_size=max(int(total / 1000), 1024 * 1024)):
+                    downloaded += len(data)
+                    f.write(data)
+                    done = int(50 * downloaded / total)
+                    sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50 - done)))
+                    sys.stdout.flush()
+        sys.stdout.write('\n')
+    except (Exception, KeyboardInterrupt):
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        raise Exception(error_msg.format(url))
+    if file_path.endswith(".zip") and unzip:
+        from zipfile import ZipFile
+        try:
+            final_path = file_path[:-4]
+            if os.path.exists(final_path):
+                print(f"The {final_path} already exists")
+                return
+            with ZipFile(file_path, 'r') as zip:
+                zip.printdir()
+                print(f'extracting {file_path}')
+                zip.extractall(file_path[:-4])
+                print(f'extracting is done!')
+            os.remove(file_path)
+        except (Exception, KeyboardInterrupt):
+            raise Exception(f"Error in extracting {file_path}")
+
+
 def download_decorator(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
@@ -80,3 +123,8 @@ def download_decorator(func):
         return func(self, *args, **kwargs)
 
     return wrapper
+
+
+if __name__ == '__main__':
+    url = 'https://codeload.github.com/opencv/opencv-python/zip/refs/tags/57'
+    download_file(url, '/home/ai/remove.zip', unzip=True)
