cb2b79158ab15a237786bc6eb5509245236ab676
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-11-14 09:49:02 +0330

    [utils][decorators] Add get_func_time

--- deep_utils/utils/lib_utils/__init__.py
+++ deep_utils/utils/lib_utils/__init__.py
@@ -1,4 +1,4 @@
 from .download_utils import download_decorator, download_file
 from .main_utils import import_module
 from .lib_decorators import get_from_config, expand_input, get_elapsed_time, rgb2bgr, cast_kwargs_dict
-from .decorators import get_method_time
+from .decorators import get_method_time, get_func_time
