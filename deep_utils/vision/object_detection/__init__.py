cc022a255dfc2f4dce59388913f5024fdadb4fcb
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-03-15 01:11:50 +0330

    [deep_utils] Fix structure design

--- deep_utils/vision/object_detection/__init__.py
+++ deep_utils/vision/object_detection/__init__.py
@@ -2,12 +2,12 @@ from .main import ObjectDetector
 from deep_utils.utils.lib_utils.main_utils import list_utils, loader
 from deep_utils.utils.lib_utils.framework_utils import is_torch_available
 
-if is_torch_available():
-    from deep_utils.vision.object_detection.yolo.v5.torch.yolo_v5_torch_object_detection import \
-        YOLOV5TorchObjectDetector
-else:
+if not is_torch_available():
     # Here go the dummy objects
     from deep_utils.dummy_objects.vision.object_detection import YOLOV5TorchObjectDetector
+else:
+    from deep_utils.vision.object_detection.yolo.v5.torch.yolo_v5_torch_object_detection import \
+        YOLOV5TorchObjectDetector
 
 Object_Detection_Models = {
     "YOLOV5TorchObjectDetector": YOLOV5TorchObjectDetector
