ed803eb5d4a6d7f579b89ee489198d14e68fe74a
pooya-mohammadi <pooyamohammadikazaj@yahoo.com>
2022-03-27 02:21:43 +0430

    [tests][yolo] Fix input image

--- tests/vision/object_detection/yolo/v5/torch/test_detection.py
+++ tests/vision/object_detection/yolo/v5/torch/test_detection.py
@@ -7,7 +7,7 @@ from deep_utils import Box, YOLOV5TorchObjectDetector, download_file
 def test_detection():
     model = YOLOV5TorchObjectDetector(model_weight="yolov5s.pt", device='cpu')
     file_path = download_file(
-        "https://raw.githubusercontent.com/pooya-mohammadi/deep_utils/master/examples/vision/data/movie-stars.jpg")
+        "https://raw.githubusercontent.com/pooya-mohammadi/deep-utils-notebooks/main/vision/images/dog.jpg")
     img = cv2.imread(file_path)
     res = model.detect_objects(img, is_rgb=False, get_time=True)
     assert len(res.boxes) >= 0, "Model did not detect anything!"
