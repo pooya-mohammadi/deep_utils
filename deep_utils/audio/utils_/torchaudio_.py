ca7a4c96e5225241e85c71bb7878f551b9c76ccc
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-05-11 00:46:41 +0430

    [audio][utils] Update torchaudio_.py's functions

--- deep_utils/audio/utils_/torchaudio_.py
+++ deep_utils/audio/utils_/torchaudio_.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from typing import Union
 import torchaudio
@@ -33,3 +34,47 @@ class TorchAudioUtils:
         else:
             log_print(logger, f"Successfully resampled wav-file {waveform.shape} with {resample_rate} sample rate!")
             return resampled_waveform
+
+    @staticmethod
+    def split(wave, sr, max_seconds: float = 10, min_seconds=1, logger=None, verbose=1) -> list[torch.Tensor]:
+        """
+        Splits a wave to mini-waves based on input max_seconds. If the last segment's duration is less than min_seconds
+        it is combined with its previous segment.
+        :param wave:
+        :param sr:
+        :param max_seconds:
+        :param min_seconds:
+        :param logger:
+        :param verbose:
+        :return:
+        """
+        wave_duration = TorchAudioUtils.get_duration(wave, sr)
+        if max_seconds is None or wave_duration < max_seconds:
+            return [wave]
+
+        unsqueeze = False
+        if len(wave.shape) == 2:
+            wave = wave.squeeze(0)
+            unsqueeze = True
+        n_intervals = math.ceil(wave_duration / max_seconds)
+        waves = []
+
+        for interval in range(n_intervals):
+            s = (interval * max_seconds) * sr
+            e = ((interval + 1) * max_seconds) * sr
+            w = wave[s:e]
+            w_duration = TorchAudioUtils.get_duration(w, sr)
+            w = w.unsqueeze(0) if unsqueeze else w
+            if w_duration < min_seconds:
+                waves[-1] = torch.concat([waves[-1], w], dim=1)
+            else:
+                waves.append(w)
+        if len(waves) > 1:
+            log_print(logger, f"Successfully split input wave to {len(waves)} waves!", verbose=verbose)
+        return waves
+
+    @staticmethod
+    def get_duration(wave, sr):
+        if len(wave.shape) == 2:
+            wave = wave.squeeze(0)
+        return wave.shape[0] / sr
