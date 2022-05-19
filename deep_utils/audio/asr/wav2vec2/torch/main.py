6d9d4dc6f72238cfc98bc4b7f527826c040aca0e
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-05-20 00:03:06 +0430

    [audio][asr][wav2vec2] Add sst_group to Wav2Vec2STTTorch

--- deep_utils/audio/asr/wav2vec2/torch/main.py
+++ deep_utils/audio/asr/wav2vec2/torch/main.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 import librosa
 import numpy as np
 import torch
@@ -12,7 +12,9 @@ class Wav2Vec2STTTorch:
         self.processor = Wav2Vec2Processor.from_pretrained(model_path)
         self.model = Wav2Vec2ForCTC.from_pretrained(model_path).eval().to(self.device)
 
-    def speech_to_text(self, speech_array) -> str:
+    def sst(self, speech_array) -> str:
+        if len(speech_array.shape) == 2:
+            speech_array = speech_array.squeeze(0)
         input_values = self.processor(speech_array, sampling_rate=self.sample_rate,
                                       return_tensors='pt').input_values.to(self.device)
         logits = self.model(input_values).logits
@@ -22,4 +24,11 @@ class Wav2Vec2STTTorch:
 
     def stt_file(self, file_path) -> Tuple[np.ndarray, float, str]:
         speech, sr = librosa.load(file_path, self.sample_rate)
-        return speech, sr, self.speech_to_text(speech)
+        return speech, sr, self.sst(speech)
+
+    def sst_group(self, audio_segments) -> List[str]:
+        texts = []
+        for audio in audio_segments:
+            text = self.sst(audio)
+            texts.append(text)
+        return texts
