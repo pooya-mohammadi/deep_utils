a09f57f48777b5f6cd2277e59bf0da3cfc3326f3
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-05-21 19:44:29 +0430

    [utils][json_utils] Add ensure_ascii for persian language

--- deep_utils/utils/utils/json_utils.py
+++ deep_utils/utils/utils/json_utils.py
@@ -1,24 +1,27 @@
-def load_json(json_path: str):
+def load_json(json_path: str, encoding="utf-8"):
     """
     :param json_path: Path to json file to load
+    :param encoding: encoding format
     :return: returns a json file
     """
     import json
-    with open(json_path) as f:
+    with open(json_path, encoding=encoding) as f:
         json_data = json.load(f)
     return json_data
 
 
-def dump_json(json_path, dict_object: dict):
+def dump_json(json_path, dict_object: dict, encoding="utf-8", ensure_ascii=True):
     """
     dumps a json file
     Args:
         dict_object:
         json_path: path to json file
+        encoding: encoding format
+        ensure_ascii: set to False for persian characters
 
     Returns:
 
     """
     import json
-    with open(json_path, mode='w') as f:
-        json.dump(dict_object, f)
+    with open(json_path, mode='w', encoding=encoding) as f:
+        json.dump(dict_object, f, ensure_ascii=ensure_ascii)
