5e9188d6d7473262a967be65b98e92104d5bb6f4
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-04 17:34:42 +0430

    [setup] Add new version 0.9.2

--- deep_utils/_version.py
+++ deep_utils/_version.py
@@ -1,3 +1,3 @@
 """Deep_utils package version."""
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.9.1"
+VERSION = "0.9.2"
 
 try:
     import pypandoc
