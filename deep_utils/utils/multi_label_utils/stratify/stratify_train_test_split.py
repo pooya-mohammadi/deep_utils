aa4e63d588b19aeac99c6d8e17ddee1c8c650da0
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-08 21:54:58 +0430

    [utils][stratify] Fix to split no_objects based on train_test ratio

--- deep_utils/utils/multi_label_utils/stratify/stratify_train_test_split.py
+++ deep_utils/utils/multi_label_utils/stratify/stratify_train_test_split.py
@@ -26,23 +26,52 @@ def stratify_train_test_split_multi_label(x: Union[list, tuple, np.ndarray], y: 
     (array([[2, 2, 2],
            [3, 3, 3]]), array([[1, 1, 1]]), array([[1, 0, 0],
            [1, 2, 0]]), array([[1, 2, 0]]))
-
+    >>> x = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
+    >>> y = np.array([[0, 0], [0, 0], [0, 1], [0, 1], [1, 1], [1, 1], [1, 0], [1, 0]])
+    >>> x_train, x_test, y_train, y_test = stratify_train_test_split_multi_label(x, y, test_size=0.5, closest_ratio=False)
+    >>> x_train
+    array([[1, 2],
+           [3, 4],
+           [1, 2],
+           [3, 4]])
+    >>> x_test
+    array([[1, 2],
+           [3, 4],
+           [1, 2],
+           [3, 4]])
+    >>> y_train
+    array([[0, 1],
+           [0, 1],
+           [1, 0],
+           [1, 0]])
+    >>> y_test
+    array([[1, 1],
+           [1, 1],
+           [0, 0],
+           [0, 0]])
+    >>> print("class ratio:", tuple(y_test.sum(0) / y.sum(0)))
+    class ratio: (0.5, 0.5)
+    >>> print("sample ratio:", y_test.shape[0] / y.shape[0])
+    sample ratio: 0.5
     """
     assert len(y.shape) == 2, "y should be 2D"
     assert test_size > 0.0, "test_size cannot be a zero or negative value!"
-    from sklearn.model_selection import train_test_split
     x = np.array(x, dtype=np.object_) if not isinstance(x, np.ndarray) else x
 
     # excluding samples with no objects/tags
-    y_no_objects = y[y.sum(1) == 0]
-    x_no_objects = x[y.sum(1) == 0]
-    x = x[y.sum(1) > 0]
-    y = y[y.sum(1) > 0]
+    non_objects = np.any(y.sum(1) == 0)
+    if non_objects:
+        y_no_objects = y[y.sum(1) == 0]
+        x_no_objects = x[y.sum(1) == 0]
+        x = x[y.sum(1) > 0]
+        y = y[y.sum(1) > 0]
 
     available_samples = np.ones((y.shape[0]), dtype=np.bool8)
     test_samples = np.zeros((y.shape[0]), dtype=np.bool8)
     train_samples = np.zeros((y.shape[0]), dtype=np.bool8)
     class_sample_counts = y.sum(axis=0)
+    ideal_train_size = np.floor(sum(class_sample_counts) * (1 - test_size))
+
     # stratify starts from a class with the lowest number of samples
     class_indices = np.argsort(class_sample_counts)
     for class_index in class_indices:
@@ -61,8 +90,6 @@ def stratify_train_test_split_multi_label(x: Union[list, tuple, np.ndarray], y: 
             chosen_indices = sorted_indices[cum_sum_values < n_test].tolist()
             if len(chosen_indices) < len(sorted_indices):
                 chosen_indices.append(sorted_indices[len(chosen_indices)])
-            # ratio = input_labels[sorted_indices[cum_sum_values < n_test]].sum()/ cum_sum_values[-1]
-            # v = 1
         # Update available_samples, train_samples, test_samples
         for update_index, n_label in enumerate(input_labels):
             if n_label == 0:
@@ -79,23 +106,19 @@ def stratify_train_test_split_multi_label(x: Union[list, tuple, np.ndarray], y: 
     # samples to the whole dataset.
     train_samples = np.bitwise_or(train_samples, np.bitwise_not(test_samples))
 
-    # splitting samples with no objects
-    x_no_objects_train, x_no_objects_test, y_no_objects_train, y_no_objects_test = train_test_split(x_no_objects,
-                                                                                                    y_no_objects,
-                                                                                                    test_size=test_size)
+    if non_objects:
+        # splitting samples with no objects trying to save the balance between train and test numbers
 
-    return np.concatenate([x[train_samples], x_no_objects_train]), \
-           np.concatenate([x[test_samples], x_no_objects_test]), \
-           np.concatenate([y[train_samples], y_no_objects_train]), \
-           np.concatenate([y[test_samples], y_no_objects_test])
+        train_left = int(ideal_train_size - sum(train_samples))
+        indices = np.arange(len(y_no_objects))
+        np.random.shuffle(indices)
 
+        x_no_objects_train, y_no_objects_train = x_no_objects[:train_left], y_no_objects[:train_left]
+        x_no_objects_test, y_no_objects_test = x_no_objects[train_left:], y_no_objects[train_left:]
 
-if __name__ == '__main__':
-    X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
-    y = np.array([[0, 0], [0, 0], [0, 1], [0, 1], [1, 1], [1, 1], [1, 0], [1, 0]])
-    x_train, x_test, y_train, y_test = stratify_train_test_split_multi_label(X, y, test_size=0.5, closest_ratio=False)
-    print("x_train", x_train)
-    print("x_test", x_test)
-    print("y_train", y_train)
-    print("y_test", y_test)
-    print("ratio:", y_test.sum(0) / y.sum(0))
+        return np.concatenate([x[train_samples], x_no_objects_train]), \
+               np.concatenate([x[test_samples], x_no_objects_test]), \
+               np.concatenate([y[train_samples], y_no_objects_train]), \
+               np.concatenate([y[test_samples], y_no_objects_test])
+    else:
+        return x[train_samples], x[test_samples], y[train_samples], y[test_samples]
