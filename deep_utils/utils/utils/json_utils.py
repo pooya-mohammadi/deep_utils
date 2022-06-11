7590614d06bd220dc6e25331c3e02f824e8ae5fa
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-12 00:00:47 +0430

    [utils][json-utils] Add output typing

--- deep_utils/utils/utils/json_utils.py
+++ deep_utils/utils/utils/json_utils.py
@@ -1,4 +1,4 @@
-def load_json(json_path: str, encoding="utf-8"):
+def load_json(json_path: str, encoding="utf-8") -> dict:
     """
     :param json_path: Path to json file to load
     :param encoding: encoding format
@@ -10,7 +10,7 @@ def load_json(json_path: str, encoding="utf-8"):
     return json_data
 
 
-def dump_json(json_path, dict_object: dict, encoding="utf-8", ensure_ascii=True):
+def dump_json(json_path, dict_object: dict, encoding="utf-8", ensure_ascii=True) -> None:
     """
     dumps a json file
     Args:
