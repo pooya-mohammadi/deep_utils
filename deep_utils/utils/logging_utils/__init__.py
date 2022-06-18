6c1640a26404f7315cae9d9a93de380cfaa165fc
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-18 21:24:27 +0430

    [utils][logging-utils] Add start-end logging decorator

--- deep_utils/utils/logging_utils/__init__.py
+++ deep_utils/utils/logging_utils/__init__.py
@@ -1,1 +1,3 @@
-from .logging_utils import get_logger, log_print, value_error_log, get_conf_matrix, get_cls_report, save_params
+from .logging_utils import (get_logger, log_print, value_error_log, get_conf_matrix, get_cls_report, save_params,
+                            start_end_logger_decorator
+                            )
--- deep_utils/utils/logging_utils/logging_utils.py
+++ deep_utils/utils/logging_utils/logging_utils.py
@@ -1,24 +1,54 @@
 import os
 import logging
 import sys
+from functools import wraps
+import inspect
 from pathlib import Path
-from typing import Union
+from typing import Union, Callable
+
+LOGGING_FORMATS = {
+    "basic": '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    "func_name": "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+
+}
 
 
-def get_logger(name: str, log_path: Union[str, Path, None] = None, remove_previous_handlers=True) -> logging.Logger:
+def start_end_logger_decorator(func) -> Callable:
     """
-    Creates a logger for a given name
+        Logs the start and end of a func
+        :param func: The func that will be measured
+        :return:
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger_ = kwargs.get("logger", None)
+        verbose = kwargs.get("verbose", 1)
+        file_path = inspect.getfile(func)
+        func_name = func.__name__
+        log_print(logger_, f"{file_path} -- func: {func_name} --> message: Starting...", verbose=verbose,
+                  get_func_name=False)
+        results = func(*args, **kwargs)
+        log_print(logger_, f"{file_path} -- func: {func_name} --> message: Done...", verbose=verbose,
+                  get_func_name=False)
+        return results
+
+    return wrapper
+
+
+def get_logger(name: str, log_path: Union[str, Path, None] = None,
+               remove_previous_handlers=True,
+               logging_format: str = "basic") -> logging.Logger:
+    """
+    Creates a logger for a given name,
     :param name: The name that logger will be created for
-    :param log_path: In which logger information will be saved
+    :param log_path: Where logger information will be saved
     :param remove_previous_handlers: If set to true, removes previous handlers to prevent repeated prints
+    :param logging_format: What format to use, default is basic. Check LOGGING_FORMATS dict for available formats.
     :return:
     """
     logger = logging.getLogger(name)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+    formatter = logging.Formatter(LOGGING_FORMATS[logging_format])
 
     if log_path:
         log_dir, file_name = os.path.split(log_path)
@@ -27,23 +57,47 @@ def get_logger(name: str, log_path: Union[str, Path, None] = None, remove_previo
         file_handler = logging.FileHandler(log_path)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
+    else:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
 
     if remove_previous_handlers:
-        logger.handlers = [logger.handlers[-1]]
+        logger.handlers = logger.handlers[-1:]
     logger.setLevel(logging.INFO)
-    print(f"[INFO] Successfully created logger for {name}" + f" in {log_path}" if log_path else "")
+    print(f"[INFO] Successfully created logger for {name}" + (f" in {log_path}" if log_path else ""))
     return logger
 
 
-def log_print(logger: Union[None, logging.Logger], message: str, log_type="info", verbose=1):
+def func_log(message, roll_back=1):
+    """Automatically log the current function details."""
+
+    # Get the previous frame in the stack, otherwise it would
+    # be this function!!!
+    code_f_back = inspect.currentframe()
+    for _ in range(roll_back):
+        code_f_back = code_f_back.f_back
+    func = code_f_back.f_code
+    # Dump the message + the name of this function to the log.
+    message = f"{func.co_filename} -- line: {func.co_firstlineno} -- func: {func.co_name} --> message: {message}"
+    return message
+
+
+def log_print(logger: Union[None, logging.Logger], message: str, log_type="info", verbose=1, get_func_name=True,
+              roll_back=2):
     """
     Logs the input messages with the given log_type. In case the logger object is not provided, prints the message.
     :param logger:
     :param message:
     :param log_type:
-    :param verbose: whether to print/log or not!
+    :param verbose: whether to print/log
+    :param get_func_name: whether to include func-name in the message
+    :param roll_back: roll back used for func-log. This number indicates how many functions should `func-log` trace back
+    to get to the main func
     :return:
     """
+    if get_func_name:
+        message = func_log(message, roll_back=roll_back)
     if log_type == 'info':
         if logger is not None and isinstance(logger, logging.Logger):
             logger.info(message)
@@ -147,3 +201,8 @@ def get_cls_report(y_pred, y_true, save_path=None, logger=None):
             f.write(report)
     log_print(logger, 'Successfully generated classification report')
     return report
+
+
+if __name__ == '__main__':
+    l = get_logger("p", log_path='log.log')
+    log_print(l, "wow", verbose=1)
