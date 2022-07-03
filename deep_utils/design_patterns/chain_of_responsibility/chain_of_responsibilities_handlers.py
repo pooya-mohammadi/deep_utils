f554d6629a7721855ce363dc92ad774740353e57
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-07-03 14:14:09 +0430

    [design-patterns][CoR] Fix Managers bug in stacking handlers

--- deep_utils/design_patterns/chain_of_responsibility/chain_of_responsibilities_handlers.py
+++ deep_utils/design_patterns/chain_of_responsibility/chain_of_responsibilities_handlers.py
@@ -2,20 +2,19 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from typing import Any, List, Tuple, Union
-
 from deep_utils.utils.logging_utils.logging_utils import log_print
 
 
 class CoRError(Exception):
     def __init__(
-        self, message: str = "", code: int = 0, output: Union[None, dict] = None
+            self, message: Union[str, None] = None, code: Union[int, None] = 0, output: Union[None, dict] = None
     ):
         """
-        This is a custom error used to break the responsibility chain that is running through the manger!
+        This is a custom error used to break the responsibility chain that is running through the manager!
         It helps to increase codes' readability; when one sees this error realizes that the process is broke because
         of an unfinished process that's handled but not in the desired and intended format
-        :param message: message send by the user, default is ""
-        :param code: code send by the user, default is 0
+        :param message: message send by the user, default is None
+        :param code: code send by the user, default is None
         :param output: A dictionary that contains output.
         """
         if output is None:
@@ -71,9 +70,9 @@ class CoRHandler(Handler):
 
 class CoRManger:
     def __init__(
-        self,
-        handlers: Union[Tuple[CoRHandler], List[CoRHandler]],
-        internal_error_code=-1,
+            self,
+            handlers: Union[Tuple[CoRHandler], List[CoRHandler]],
+            internal_error_code=-1,
     ):
         """
         The default manger, which takes in a list of handlers and process it
@@ -82,16 +81,16 @@ class CoRManger:
         is used for internal errors, don't use it as a code in the handlers to prevent confusion.
         """
         assert handlers, "handlers can not be None or empty"
-        previous_handler: Union[CoRHandler, None] = None
-        for handler in handlers:
+        next_handler: Union[CoRHandler, None] = None
+        for handler in handlers[::-1]:
             assert isinstance(
                 handler, CoRHandler
             ), "Handlers should have CoRHandler type"
-            if previous_handler is not None:
-                previous_handler.set_next(handler)
-            previous_handler = handler
+            if next_handler is not None:
+                handler.set_next(next_handler)
+            next_handler = handler
 
-        self.handler: CoRHandler = previous_handler
+        self.handler: CoRHandler = handler
         self.internal_error_code = internal_error_code
 
     def manage(self, request: Any, logger=None, verbose=1):
@@ -99,15 +98,17 @@ class CoRManger:
             output = self.handler.handle(request)
         except CoRError as e:
             output = dict(e.output)
-            output["code"] = e.code
-            output["message"] = e.message
+            if e.code:
+                output["code"] = e.code
+            if e.message:
+                output["message"] = e.message
         except BaseException as e:
             output = dict()
             output["code"] = self.internal_error_code
-            output["message"] = "Internal Error: " + str(e)
+            output["message"] = f"Internal Error --> type: {type(e)}, msg: {e}"
         log_print(
             logger,
-            f"Request code: {output['code']}, message: {output['message']}!",
+            f"Request code: {output.get('code', 0)}, message: {output.get('message', '')}",
             verbose=verbose,
         )
         return output
