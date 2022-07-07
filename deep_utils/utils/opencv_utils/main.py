c7a802aeac57ca6ce8f8ebdcd2c8a34f861a9c99
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-07-07 20:58:07 +0430

    [utils][opencv-utils] Add if to show_destroy_cv2

--- deep_utils/utils/opencv_utils/main.py
+++ deep_utils/utils/opencv_utils/main.py
@@ -72,13 +72,13 @@ def translate(img, tx, ty, dsize=None):
     return translated_image
 
 
-def show_destroy_cv2(img, win_name=""):
+def show_destroy_cv2(img, win_name="", show=True):
     import cv2
-
-    try:
-        cv2.imshow(win_name, img)
-        cv2.waitKey(0)
-        cv2.destroyWindow(win_name)
-    except Exception as e:
-        cv2.destroyWindow(win_name)
-        raise e
+    if show:
+        try:
+            cv2.imshow(win_name, img)
+            cv2.waitKey(0)
+            cv2.destroyWindow(win_name)
+        except Exception as e:
+            cv2.destroyWindow(win_name)
+            raise e
