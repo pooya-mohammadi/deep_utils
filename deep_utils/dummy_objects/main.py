91771d193626b59fd003793c9e90b651e4fa0c56
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-07-17 14:01:29 +0430

    [dummy-objects] Remove get_item

--- deep_utils/dummy_objects/main.py
+++ deep_utils/dummy_objects/main.py
@@ -21,6 +21,4 @@ class DummyObject(type):
     """
 
     def __getattr__(cls, key):
-        if key.startswith("_"):
-            return super().__getattr__(cls, key)
-        requires_backends(cls, cls._backends)
+        requires_backends(cls, cls._backend)
