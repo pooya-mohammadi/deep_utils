8b0d713080d567f392e930e4799a5b5e33249493
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-08-23 15:24:33 +0430

    [nlp][ner] Update combine_tokens_with_hashtag_ner to have an argument to ignore inputs_labels equality

--- deep_utils/nlp/ner/utils_.py
+++ deep_utils/nlp/ner/utils_.py
@@ -26,11 +26,14 @@ def check_bio_labels(input_labels: Union[str, List[str], Tuple[str]], query_labe
     return False
 
 
-def combine_tokens_with_hashtag_ner(input_words: List[str], input_labels: List[str]):
+def combine_tokens_with_hashtag_ner(input_words: List[str], input_labels: List[str], ignore_labels_equality=False):
     """
-    This function connects tokenized words with # to each other if they have the same labels
+    This function connects tokenized words with # to each other if they have the same labels. In case you want to
+    connect words with # to each other without considering labels' equality, set ignore_labels_equality to True.
     :param input_words:
     :param input_labels:
+    :param ignore_labels_equality: if set to True, ignores the labels' equality condition in the process of connecting
+    terms with #s
     :return:
     >>> words =  ['تلفن', '98', '##21', '##55', '##87', '##14', '##42']
     >>> labels = [ 'B-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone']
@@ -40,17 +43,24 @@ def combine_tokens_with_hashtag_ner(input_words: List[str], input_labels: List[s
     >>> labels = [ 'B-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone'] + ["o"] + [ 'B-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone']
     >>> combine_tokens_with_hashtag_ner(words, labels)
     (['تلفن', '982155871442', ',', 'تلفن', '982155871442'], ['B-phone', 'I-phone', 'o', 'B-phone', 'I-phone'])
+    >>> # ignore_labels_equality samples
+    >>> words =  ['تلفن', '98', '##21', '##55', '##87', '##14', '##42'] + [","] + ['تلفن', '98', '##21', '##55', '##87', '##14', '##42']
+    >>> labels = [ 'B-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone'] + ["o"] + [ 'B-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone', 'I-phone']
+    >>> combine_tokens_with_hashtag_ner(words, labels, ignore_labels_equality=True)
+    (['تلفن', '982155871442', ',', 'تلفن', '982155871442'], ['B-phone', 'I-phone', 'o', 'B-phone', 'I-phone'])
+    >>> words = ["شهید", "##قندی", "نیلوفر", "خیابان","شهید","دکتر","بهشتی","خیابان","سهروردی","شمالی","پلاک", "497"]
+    >>> labels = ["B-section", "B-street", "B-street", "B-street", "I-street", "I-street", "I-street", "B-street", "I-street", "I-street", "B-house_number", "I-house_number" ]
+    >>> combine_tokens_with_hashtag_ner(words, labels, ignore_labels_equality=True)
+    (['شهیدقندی', 'نیلوفر', 'خیابان', 'شهید', 'دکتر', 'بهشتی', 'خیابان', 'سهروردی', 'شمالی', 'پلاک', '497'], ['B-section', 'B-street', 'B-street', 'I-street', 'I-street', 'I-street', 'B-street', 'I-street', 'I-street', 'B-house_number', 'I-house_number'])
     """
-    assert len(input_words) == len(
-        input_labels
-    ), "[Error] Number of input_words and input_labels is not equal"
+    assert len(input_words) == len(input_labels), "[Error] Number of input_words and input_labels is not equal"
     res_words, res_labels = [], []
     sub_word = ""
     sub_label = None
     save_label = None
     for word, label in zip(input_words, input_labels):
         query_label = label.replace("I-", "").replace("B-", "")
-        if "#" in word and (query_label == sub_label or not sub_label):
+        if "#" in word and ((query_label == sub_label or ignore_labels_equality) or not sub_label):
             sub_word += word.replace("#", "")
             sub_label = query_label
         else:
