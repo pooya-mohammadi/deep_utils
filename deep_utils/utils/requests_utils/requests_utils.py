e0b7afae893011d82887ae4eb50b6205c1361c93
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-09-22 13:58:40 +0330

    [utils][requests_utils] Return dictionary

--- deep_utils/utils/requests_utils/requests_utils.py
+++ deep_utils/utils/requests_utils/requests_utils.py
@@ -1,7 +1,8 @@
-def get_request(ip, down_message="Down"):
+def get_request(ip, down_key, down_message="Down") -> dict:
     """
     This simple function wraps the get request
     :param ip:
+    :param down_key: key of the output if it fails
     :param down_message:
     :return:
     """
@@ -9,5 +10,5 @@ def get_request(ip, down_message="Down"):
     try:
         status = requests.get(ip).json()
     except:
-        status = down_message
+        status = {down_key: down_message}
     return status
