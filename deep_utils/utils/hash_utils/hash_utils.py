1e135c62f9c190757565733acab11aaf3ff487c1
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-10-08 15:36:19 +0330

    [utils][hash] combine file based with file obj in

--- deep_utils/utils/hash_utils/hash_utils.py
+++ deep_utils/utils/hash_utils/hash_utils.py
@@ -1,16 +1,24 @@
-def get_hash_file(file_path, buffer_size=65536):
+def get_hash_file(file, buffer_size=65536):
     """
     This function converts a file in chunk mode to hash file!
     buffer_size = 65536 lets read stuff in 64kb chunks
-    :param file_path:
+    :param file: str, io.BufferedReader
     :param buffer_size:
     :return:
     """
     import hashlib
     hash_obj = hashlib.sha512()
-    with open(file_path, 'rb') as f:
+    if isinstance(file, str):
+        with open(file, 'rb') as f:
+            while True:
+                data = f.read(buffer_size)
+                if not data:
+                    break
+                hash_obj.update(data)
+    else:
+        hash_obj = hashlib.sha512()
         while True:
-            data = f.read(buffer_size)
+            data = file.read(buffer_size)
             if not data:
                 break
             hash_obj.update(data)
