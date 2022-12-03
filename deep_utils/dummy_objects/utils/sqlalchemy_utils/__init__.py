d724e94997602ec05160eb462f02b9212bfcd4f0
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-12-03 12:46:52 +0330

    [utils][sqlalchemy] Add inserts

--- deep_utils/dummy_objects/utils/sqlalchemy_utils/__init__.py
+++ deep_utils/dummy_objects/utils/sqlalchemy_utils/__init__.py
@@ -6,3 +6,10 @@ class SQLAlchemyChecks(metaclass=DummyObject):
 
     def __init__(self, *args, **kwargs):
         requires_backends(self, self._backend)
+
+
+class SQLAlchemyInserts(metaclass=DummyObject):
+    _backend = ["sqlalchemy"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, self._backend)
