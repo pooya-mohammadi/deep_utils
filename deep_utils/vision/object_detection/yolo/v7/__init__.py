a8f990df30f4b2a44e37c692922dea7f19570e77
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-12-07 11:21:38 +0330

    [vision][object-detection] Add new yolov5 version

---
+++ deep_utils/vision/object_detection/yolo/v7/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from deep_utils.dummy_objects.vision.object_detection import YOLOV7TorchObjectDetector
+    from deep_utils.vision.object_detection.yolo.v7.torch.yolo_v7_torch_object_detection import (
+        YOLOV7TorchObjectDetector,
+    )
+except ModuleNotFoundError:
+    pass
+except Exception as e:
+    raise e
