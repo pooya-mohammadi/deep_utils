a577cc9825685bd1dc557ee294ebc4151612043b
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-03-24 19:19:10 +0430

    [vision][face-detection] Fix MTCNNTFFaceDetector import

--- deep_utils/vision/__init__.py
+++ deep_utils/vision/__init__.py
@@ -1,7 +1,7 @@
-from .face_detection import *
-from .object_detection import *
-from .object_tracker import *
-from .torch_vision import *
-from .face_recognition import *
-from .vision_utils import *
-from .ocr import *
+# from .face_detection import *
+# from .object_detection import *
+# from .object_tracker import *
+# from .torch_vision import *
+# from .face_recognition import *
+# from .vision_utils import *
+# from .ocr import *
--- deep_utils/vision/face_detection/__init__.py
+++ deep_utils/vision/face_detection/__init__.py
@@ -1,23 +1,23 @@
-from deep_utils.utils.lib_utils.main_utils import list_utils, loader
-from .haarcascade import *
-from .mtcnn import *
-from .retinaface import *
-from .ssd import *
-from .ultralight import *
-from .main import *
-
-Face_Detection_Models = {
-    "SSDCV2CaffeFaceDetector": SSDCV2CaffeFaceDetector,
-    "MTCNNTorchFaceDetector": MTCNNTorchFaceDetector,
-    "HaarcascadeCV2FaceDetector": HaarcascadeCV2FaceDetector,
-    "MTCNNTFFaceDetector": MTCNNTFFaceDetector,
-    "UltralightTorchFaceDetector": UltralightTorchFaceDetector,
-    # "UltralightTFFaceDetector": UltralightTFFaceDetector,
-    "RetinaFaceTorchFaceDetector": RetinaFaceTorchFaceDetector,
-}
-
-list_face_detection_models = list_utils(Face_Detection_Models)
-
-
-def face_detector_loader(name, **kwargs) -> FaceDetector:
-    return loader(Face_Detection_Models, list_face_detection_models)(name, **kwargs)
+# from deep_utils.utils.lib_utils.main_utils import list_utils, loader
+# from .haarcascade import *
+# from .mtcnn import *
+# from .retinaface import *
+# from .ssd import *
+# from .ultralight import *
+# from .main import *
+#
+# Face_Detection_Models = {
+#     "SSDCV2CaffeFaceDetector": SSDCV2CaffeFaceDetector,
+#     "MTCNNTorchFaceDetector": MTCNNTorchFaceDetector,
+#     "HaarcascadeCV2FaceDetector": HaarcascadeCV2FaceDetector,
+#     "MTCNNTFFaceDetector": MTCNNTFFaceDetector,
+#     "UltralightTorchFaceDetector": UltralightTorchFaceDetector,
+#     # "UltralightTFFaceDetector": UltralightTFFaceDetector,
+#     "RetinaFaceTorchFaceDetector": RetinaFaceTorchFaceDetector,
+# }
+#
+# list_face_detection_models = list_utils(Face_Detection_Models)
+#
+#
+# def face_detector_loader(name, **kwargs) -> FaceDetector:
+#     return loader(Face_Detection_Models, list_face_detection_models)(name, **kwargs)
