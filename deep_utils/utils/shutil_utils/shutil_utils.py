05c00ffd39db3d3cfbe9655b8a060f6e9be4e14d
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-03-25 19:21:13 +0430

    [utils][shutil_utils] Add doc-string and annotation

--- deep_utils/utils/shutil_utils/shutil_utils.py
+++ deep_utils/utils/shutil_utils/shutil_utils.py
@@ -3,14 +3,28 @@ from os.path import join, split
 from typing import List
 
 
-def mv_or_copy(src, dst, mode):
+def mv_or_copy(src: str, dst: str, mode: str):
+    """
+    Move or copy file using shutil library
+    :param src:
+    :param dst:
+    :param mode:
+    :return:
+    """
     if mode == "cp":
         shutil.copy(src, dst)
     elif mode == "mv":
         shutil.move(src, dst)
 
 
-def mv_or_copy_list(src_lst: List[str], dst: str, mode):
+def mv_or_copy_list(src_lst: List[str], dst: str, mode: str):
+    """
+    Move or copy a list of files
+    :param src_lst:
+    :param dst:
+    :param mode:
+    :return:
+    """
     if mode == "cp":
         for src in src_lst:
             src_name = split(src)[1]
