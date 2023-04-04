dfae5bce1f75fbe414f48f3d14c9ef502a90198b
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-04-04 19:03:51 +0430

    [utils][torch_utils] Add kwargs to save_config_to_weight

--- deep_utils/utils/torch_utils/torch_utils.py
+++ deep_utils/utils/torch_utils/torch_utils.py
@@ -5,10 +5,17 @@ from deep_utils.utils.object_utils.object_utils import get_obj_variables
 
 class TorchUtils:
     @staticmethod
-    def save_config_to_weight(weight_path, config, logger=None, verbose=1):
+    def save_config_to_weight(weight_path, config, logger=None, verbose=1, **kwargs):
 
         best_weight = torch.load(weight_path)
         for k, v in get_obj_variables(config).items():
+            if k not in best_weight:
+                best_weight[k] = v
+            else:
+                log_print(logger, f"[Warning] Did not save {k} = {v} because there is a variable with the same name!",
+                          verbose=verbose)
+        # Add kwargs
+        for k, v in kwargs.items():
             if k not in best_weight:
                 best_weight[k] = v
             else:
