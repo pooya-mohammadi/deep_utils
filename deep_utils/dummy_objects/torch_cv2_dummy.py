dd9e8a02359bbb8ceeef21a83c614e45c093d81a
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-04-15 15:45:47 +0430

    [dummy][cv2_torch] Add YOLOV5TorchObjectDetector

--- deep_utils/dummy_objects/torch_cv2_dummy.py
+++ deep_utils/dummy_objects/torch_cv2_dummy.py
@@ -3,7 +3,15 @@ from deep_utils.dummy_objects.dummy_framework import DummyObject, requires_backe
 
 class MTCNNTorchFaceDetector(metaclass=DummyObject):
     _backend = ["torch", "cv2"]
-    _module = "deep_utils.vision.face_detection.mtcnn.torch.mtcnn_torch_face_detection "
+    _module = "deep_utils.vision.face_detection.mtcnn.torch.mtcnn_torch_face_detection"
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, self._backend, module_name=self._module, cls_name=self.__class__.__name__)
+
+
+class YOLOV5TorchObjectDetector(metaclass=DummyObject):
+    _backend = ["torch", "cv2"]
+    _module = "deep_utils.vision.object_detection.yolo.v5.torch.yolo_v5_torch_object_detection"
 
     def __init__(self, *args, **kwargs):
         requires_backends(self, self._backend, module_name=self._module, cls_name=self.__class__.__name__)
