489fb30d4880f552ba7e9cb85b08e623f36bbdb2
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-04-15 15:46:16 +0430

    [dummy][cv2_torch] Add YOLOV7TorchObjectDetector

--- deep_utils/dummy_objects/torch_cv2_dummy.py
+++ deep_utils/dummy_objects/torch_cv2_dummy.py
@@ -15,3 +15,11 @@ class YOLOV5TorchObjectDetector(metaclass=DummyObject):
 
     def __init__(self, *args, **kwargs):
         requires_backends(self, self._backend, module_name=self._module, cls_name=self.__class__.__name__)
+
+
+class YOLOV7TorchObjectDetector(metaclass=DummyObject):
+    _backend = ["torch", "cv2"]
+    _module = "deep_utils.vision.object_detection.yolo.v7.torch.yolo_v7_torch_object_detection"
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, self._backend, module_name=self._module, cls_name=self.__class__.__name__)
