ef25b19373c3c60ed082885b174b9e56d11700ef
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-05-27 20:48:41 +0330

    Add from_list and to_list to box_dataclasses.py

--- deep_utils/utils/box_utils/box_dataclasses.py
+++ deep_utils/utils/box_utils/box_dataclasses.py
@@ -12,3 +12,10 @@ class PointDataClass:
 class BoxDataClass:
     p1: PointDataClass
     p2: PointDataClass
+
+    @classmethod
+    def from_list(cls, box_list):
+        return cls(PointDataClass(box_list[0], box_list[1]), PointDataClass(box_list[2], box_list[3]))
+
+    def to_list(self):
+        return [self.p1.x, self.p1.y, self.p2.x, self.p2.y]
