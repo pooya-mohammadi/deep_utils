f3bc27fbc876f974a25f5b5c144a33a84530b322
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2023-11-26 11:47:54 +0330

    Fix max_val in swap_seg_value

--- deep_utils/__init__.py
+++ deep_utils/__init__.py
@@ -4,7 +4,7 @@ from deep_utils.dummy_objects.dummy_framework import LazyModule
 from .utils.lib_utils.integeration_utils import import_lazy_module
 
 # Deep Utils version number
-__version__ = "1.3.4"
+__version__ = "1.3.5"
 
 from .utils.constants import DUMMY_PATH, Backends
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "1.3.4"
+VERSION = "1.3.5"
 
 long_description = open("Readme.md", mode="r", encoding="utf-8").read()
 
