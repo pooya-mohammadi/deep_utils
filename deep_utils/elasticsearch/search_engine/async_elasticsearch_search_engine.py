7c2b6396bcb95d14534c038e2b848bb2ed5dd80e
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-01-13 15:56:49 +0330

    Add search_match_query to async_elasticsearch_search_engine.py

--- deep_utils/elasticsearch/search_engine/async_elasticsearch_search_engine.py
+++ deep_utils/elasticsearch/search_engine/async_elasticsearch_search_engine.py
@@ -28,3 +28,69 @@ class AsyncElasticsearchEngin:
         )
         return resp
 
+    @staticmethod
+    def get_match_query(field_name="",
+                        field_value="",
+                        keyword="match",
+                        ):
+        """
+        This function is used to get query-match. It will simply return query-match json
+        :param field_name:
+        :param field_value:
+        :param keyword:
+        :return:
+        """
+        if keyword == "match":
+
+            query = {
+                keyword: {
+                    field_name: field_value
+                }
+            }
+        elif keyword == "match_all":
+            query = {
+                keyword: {}
+            }
+        else:
+            raise ValueError(f"keyword: {keyword} is not valid!")
+        return query
+
+    @staticmethod
+    def get_hits(results, return_source: bool = False):
+        """
+        This is a simple method to extract hits or return none when the output of the search has no hits
+        :param results:
+        :param return_source: If set to true the _source keyword will be returned!
+        :return:
+        """
+        hits = results['hits']['hits']
+        if len(hits) == 0:
+            hits = None
+        elif return_source:
+            hits = [hit["_source"] for hit in hits]
+        return hits
+
+    async def search_match_query(self, index_name, field_name="", field_value="", keyword="match", size=None,
+                                 return_source: bool = False):
+        """
+        A simple match search
+        {
+        "query": {
+            "keyword": {
+                "field_name": "field_value"
+                     }
+                 }
+         }
+        :param index_name:
+        :param field_name:
+        :param field_value:
+        :param keyword:
+        :param size:
+        :param return_source:
+        :return:
+
+        """
+        query = self.get_match_query(field_name=field_name, field_value=field_value, keyword=keyword)
+        results = await self.es.search(index=index_name, query=query, size=size)
+        hits = AsyncElasticsearchEngin.get_hits(results.body, return_source=return_source)
+        return hits
