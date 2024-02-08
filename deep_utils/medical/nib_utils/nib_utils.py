772e6255e9ccee53b12845aa226e4930a6b4d03d
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-02-09 02:30:28 +0330

    Add NIBUtils

--- deep_utils/medical/nib_utils/nib_utils.py
+++ deep_utils/medical/nib_utils/nib_utils.py
@@ -1,2 +1,32 @@
+from typing import Union
+
+import numpy as np
+import nibabel as nib
+from deep_utils import SITKUtils
+from nibabel.filebasedimages import FileBasedImage
+
+
 class NIBUtils:
-    pass
+    @staticmethod
+    def get_img(filepath: str) -> FileBasedImage:
+        img = nib.load(filepath)
+        return img
+
+    @staticmethod
+    def get_array(filepath: str) -> np.ndarray:
+        img = NIBUtils.get_img(filepath)
+        array = img.get_fdata()
+        return array
+
+    @staticmethod
+    def get_img_array(filepath: str) -> Union[np.ndarray, FileBasedImage]:
+        img = NIBUtils.get_img(filepath)
+        array = img.get_fdata()
+        return array, img
+
+    @staticmethod
+    def save_sample(filepath: str, sample_array, affine, header):
+        clipped_img = nib.Nifti1Image(sample_array, affine, header)
+        nib.save(clipped_img, filepath)
+
+
