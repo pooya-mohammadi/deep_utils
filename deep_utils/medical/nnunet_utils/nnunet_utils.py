7ff3979e99eaff022d617a262330c0866839a78a
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-02-19 21:25:20 +0330

    Add nnunet_utils

--- deep_utils/medical/nnunet_utils/nnunet_utils.py
+++ deep_utils/medical/nnunet_utils/nnunet_utils.py
@@ -11,11 +11,11 @@ class NNUnetUtils:
 
         lbl_dir = img_dir.replace("images", "labels")
         path = join(path, lbl_dir)
-        base_pattern = r"(\w+)_\d+(\.[\w\.]+)"
+        base_pattern = r"([\w\-]+)_\d+(\.[\w\.]+)"
         base_match = re.match(base_pattern, identifier)
         lbl_identifier = f"{base_match.group(1)}{base_match.group(2)}"
         return join(path, lbl_identifier)
 
 
 if __name__ == '__main__':
-    print(NNUnetUtils.get_lbl_from_img_path("pooya/p_0001.nii.gz"))
+    print(NNUnetUtils.get_lbl_from_img_path( "/home/ai/projects/mm_data/nnunet_raw/Dataset005_mm/imagesTr/001_SA-1_0000.nii.gz"))
