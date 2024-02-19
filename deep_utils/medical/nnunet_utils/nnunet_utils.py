6cd11bfd76c142f30f4c81409fb4c3590b35693c
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-02-19 23:56:18 +0330

    Add nnunet_utils

--- deep_utils/medical/nnunet_utils/nnunet_utils.py
+++ deep_utils/medical/nnunet_utils/nnunet_utils.py
@@ -11,11 +11,12 @@ class NNUnetUtils:
 
         lbl_dir = img_dir.replace("images", "labels")
         path = join(path, lbl_dir)
-        base_pattern = r"([\w\-\%]+)_\d+(\.[\w\.]+)"
+        base_pattern = r"([\w\-\%\,]+)_\d+(\.[\w\.]+)"
         base_match = re.match(base_pattern, identifier)
         lbl_identifier = f"{base_match.group(1)}{base_match.group(2)}"
         return join(path, lbl_identifier)
 
 
 if __name__ == '__main__':
-    print(NNUnetUtils.get_lbl_from_img_path('/media/aici/11111bdb-a0c7-4342-9791-36af7eb70fc01/NNUNET_OUTPUT/nnunet_raw/Dataset008_ct_bern/imagesTr/TAVI_00203_20200702172417_Fl_CaSc_Th_LF_1.0_I70f_3_55%_18_Fl_CaSc_Th_LF_1.0_I70f_3_55%_TAVI_00203_0_Si_TAVI_00203_0000.nii.gz'))
+    sample = "/media/aici/11111bdb-a0c7-4342-9791-36af7eb70fc01/NNUNET_OUTPUT/nnunet_raw/Dataset008_ct_bern/imagesTr/TAVI_00556_20230517163114_CaSc_FL_1,00_Br40_Q2_30%_Th_4_CaSc_FL_1,00_Br40_Q2_30%_Th_TAVI_00556_0_Si_TAVI_00556_0000.nii.gz"
+    print(NNUnetUtils.get_lbl_from_img_path(sample))
