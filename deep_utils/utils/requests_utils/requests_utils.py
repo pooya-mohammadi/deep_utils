af444c93a14ba0853ec302d86a7aef63534647cc
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-06-08 14:09:55 +0200

    Add io-http

--- deep_utils/utils/requests_utils/requests_utils.py
+++ deep_utils/utils/requests_utils/requests_utils.py
@@ -1,4 +1,7 @@
-from typing import Union
+import json
+from typing import Union, Optional, Dict, Any, Callable, List
+
+import aiohttp
 
 
 def get_request(ip, down_key, down_message="Down") -> dict:
@@ -68,3 +71,43 @@ async def post_form_upload(input_url, data_key: str, upload_file):
         response = await client.post(input_url, files={
             data_key: (upload_file.filename, upload_file.file, upload_file.content_type)})
         return response.json()
+
+
+class Requests:
+    def __init__(self, json_serialize: Callable = json.dumps):
+        """
+
+        :param json_serialize:  ujson.dumps is also acceptable which is a bit faster but might be incompatible!
+        """
+        self.session = aiohttp.ClientSession(json_serialize=json_serialize)  # creating session
+
+    async def form_post_async(
+            self,
+            url: str,
+            data: Optional[Dict[str, Any]],
+            files: Optional[List[Dict[str, Any]]] = None,
+            ssl: bool = False,
+            encoding: Optional[str] = None
+    ):
+        """
+        The files should have the following items:
+        [{'name': variable_name, 'value': file_content, 'content_type': multipart/form-data||application/vnd.ms-excel||etc,
+        }, {...}]
+        :param url:
+        :param data:
+        :param files:
+        :param ssl:
+        :param encoding:
+        :return:
+        """
+        form_data = aiohttp.FormData()
+        if data is not None:
+            for key, value in data.items():
+                form_data.add_field(key, str(value))
+        if files is not None:
+            for file_obj in files:
+                form_data.add_field(file_obj.get("name", "file"), file_obj["value"],
+                               filename=file_obj.get("filename"),
+                               content_type=file_obj.get("content_type", 'multipart/form-data'))
+        output = await self.session.post(url, data=data)
+        return output
