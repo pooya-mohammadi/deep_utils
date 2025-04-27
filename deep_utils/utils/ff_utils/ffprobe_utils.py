d7d969c90564f76aa72fa75507629287cda347ef
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2025-04-27 14:55:47 +0200

    Add ffprobe

--- deep_utils/utils/ff_utils/ffprobe_utils.py
+++ deep_utils/utils/ff_utils/ffprobe_utils.py
@@ -1,4 +1,6 @@
 import subprocess
+from typing import Tuple, Union
+
 
 
 class FFProbeUtils:
@@ -16,6 +18,78 @@ class FFProbeUtils:
         return round(fps_vid)
 
 
+    @staticmethod
+    def get_video_size_url(dl_url) -> Tuple[int, int]:
+
+        command = [
+            'ffprobe',
+            '-v',
+            'error',
+            '-select_streams',
+            'v:0',
+            '-show_entries',
+            'stream=width,height',
+            '-of',
+            'csv=p=0',
+            dl_url
+        ]
+
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            footage_size = result.stdout.strip().strip(",")
+            width, height = [round(item.strip()) for item in footage_size.split(",")][:2]
+            return width, height
+        else:
+            raise ValueError(f"dl_url: {dl_url} not working")
+
+    @staticmethod
+    def get_width_height_fps(dl_url:str) -> Tuple[int, int, int]:
+
+        command = [
+            'ffprobe',
+            '-v',
+            'error',
+            '-select_streams',
+            'v:0',
+            '-show_entries',
+            'stream=width,height,r_frame_rate',
+            '-of',
+            'csv=p=0',
+            dl_url
+        ]
+
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            footage_size = result.stdout.strip().strip(",")
+            width, height, fps = [round(float(item.strip().split("/")[0])) for item in footage_size.split(",")][:3]
+            return width, height, fps
+        else:
+            raise ValueError(f"dl_url: {dl_url} not working")
+
+    @staticmethod
+    def get_width_height_fps_duration_frame_count(dl_url: str) -> Tuple[int, int, int, float, int]:
+
+        command = [
+            'ffprobe',
+            '-v',
+            'error',
+            '-select_streams',
+            'v:0',
+            '-show_entries',
+            'stream=width,height,r_frame_rate,duration',
+            '-of',
+            'csv=p=0',
+            dl_url
+        ]
+
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            footage_size = result.stdout.strip().strip(",")
+            width, height, fps, duration = [round(float(item.strip().split("/")[0])) if index < 3 else float(item.strip().split("/")[0]) for index, item in enumerate(footage_size.split(",")) ][:4]
+            return width, height, fps, duration, round(duration * fps)
+        else:
+            raise ValueError(f"dl_url: {dl_url} not working")
+
 if __name__ == '__main__':
-    fps = FFProbeUtils.get_fps("https://filmeditor.io/thumbnails/saeed-video/InformativeShortFormAnimation_DEAR.mp4")
-    print("fps: ", fps)
+    info = FFProbeUtils.get_width_height_fps_duration_frame_count("/home/aicvi/projects/ShifterRemotion/public/sample.mp4")
+    print("info: ", info)
