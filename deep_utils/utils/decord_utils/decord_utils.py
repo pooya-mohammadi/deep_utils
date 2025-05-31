54f49d69965547e13203635d0b536762ee8a675e
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2025-05-31 20:15:07 +0200

    Add items to decord_utils.py

--- deep_utils/utils/decord_utils/decord_utils.py
+++ deep_utils/utils/decord_utils/decord_utils.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Union, Tuple
+from typing import List, Optional, Union, Tuple, Any
 
 import decord
 import numpy as np
@@ -10,11 +10,13 @@ class DecordUtils:
 
     @staticmethod
     def get_fps(video_path: Optional[str] = None, vr: Optional[decord.VideoReader] = None,
-                round_output: bool = True) -> int | float:
+                round_output: bool = True, return_vr: bool=False) -> Union[int | float | Tuple[int | float, Any]]:
         """
         Get video fps using decord
         :param video_path: path to video file
         :param vr: decord video reader
+        :param round_output:
+        :param return_vr:
         :return:
         """
 
@@ -26,6 +28,8 @@ class DecordUtils:
         output = vr.get_avg_fps()
         if round_output:
             output = round(output)
+        if return_vr:
+            return output, vr
         del vr
         return output
 
@@ -120,20 +124,31 @@ class DecordUtils:
         return output
 
     @staticmethod
-    def crop_video(video_path: str, start: str, end: str, output_video_path: str):
+    def crop_video(video_path: str, start: str | float = "00:00:00", end: str | float= None, output_video_path: str = None, reverse: bool = False):
         """
 
         :param video_path:
         :param output_video_path:
         :param start: "01:09:23"
         :param end: "01:56:00"
+        :param reverse:
         :return:
         """
         from deep_utils.utils.opencv_utils.opencv_utils import VideoWriterCV
         from deep_utils.utils.ff_utils.ffprobe_utils import FFProbeUtils
+        if end is None:
+            end = DecordUtils.get_video_duration(video_path)
+
+        if output_video_path is None:
+            raise ValueError("output_video_path cannot be None!")
 
         with open(video_path, mode="rb") as f:
             vr = decord.VideoReader(f)  # noqa
+
+        if isinstance(end, (float, int)):
+            end = DateTimeUtils.parse_str_time(end)
+        if isinstance(start, (float, int)):
+            start = DateTimeUtils.parse_str_time(start)
         num_frames = DecordUtils.count_video_frames(video_path=None, vr=vr)
 
         width, height, fps = FFProbeUtils.get_width_height_fps(video_path)
@@ -148,15 +163,32 @@ class DecordUtils:
             frame_indices.append(item)
         frames = DecordUtils.read_video_indices(frame_indices, vr=vr)
         vw = VideoWriterCV(output_video_path, height, width, 'mp4v', fps=fps, colorful=True)
+
+        if reverse:
+            frames = frames[::-1]
+
         for frame in frames:
             vw.write(frame[..., ::-1])
+
         vw.release()
 
     @staticmethod
-    def get_all_frames(video_path: str) -> np.ndarray:
+    def get_all_frames(video_path: str, return_vr: bool=False) -> Union[np.ndarray, Tuple[np.ndarray, Any]]:
         n_frames, vr = DecordUtils.count_video_frames(video_path, return_vr=True)
         frames = DecordUtils.read_video_indices(list(range(n_frames)), vr=vr)
+        if return_vr:
+            return frames, vr
         return frames
+
+    @staticmethod
+    def get_video_duration(video_path: str, output_in_string: bool = False):
+        fps = DecordUtils.get_fps(video_path)
+        all_frames = DecordUtils.count_video_frames(video_path)
+        seconds = all_frames/fps
+        if output_in_string:
+            return DateTimeUtils.parse_str_time(seconds)
+        else:
+            return seconds
 
 
 if __name__ == '__main__':
@@ -166,7 +198,8 @@ if __name__ == '__main__':
     # DecordUtils.crop_video("/home/aicvi/Downloads/Emily_in_Paris_S01E02_10bit_x265_1080p_WEB-DL_30nama_30NAMA.mkv",
     #                        s_time, e_time,
     #                        "demo.mp4")
-    output = DecordUtils.get_fps(
-        "/media/aicvi/11111bdb-a0c7-4342-9791-36af7eb70fc0/pooya/cloth/vivid-sister/emily/Emily_in_Paris_S01E10_10bit_x265_1080p_WEB-DL_30nama_30NAMA.mkv",
-        round_output=False)
-    print(output)
+    vid_path = "/media/aicvi/11111bdb-a0c7-4342-9791-36af7eb70fc0/pooya/cloth/vivid-sister/emily/S01E09_data/Emily_in_Paris_S01E09_10bit_x265_1080p_WEB-DL_30nama_30NAMA-Scene-025/agnostic/vid_00.mp4"
+    vid_duration = DecordUtils.get_video_duration(vid_path)
+    half = vid_duration/2
+    output = DecordUtils.crop_video(vid_path, end=half, reverse=True, output_video_path="here.mp4")
+    # print(output)
